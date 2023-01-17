d2b7ca05273846c7dc8c805d9d5bc82a5b175322
simla04 <jessicasowi@gmail.com>
2023-01-17 17:04:46 +0100

    .

--- assets/main.js
+++ assets/main.js
@@ -9,26 +9,25 @@ Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 */
 var app = new Vue({
-    el: '#app',
+    el: "#app",
     data: {
         emailArray: [],
         emailSingola: ''
     },
     mounted() {
 
-        for(let i = 0; i < 10; i++){
+        for(i = 0; i < 10; i++){
 
             // axios.get-> richiama i dati di una API; in questo caso richiama delle mail autogenerate.
             // .then( () => serve a recuperare correttamente il this)
 
 
-        axios.get( 'https://flynn.boolean.careers/exercises/api/random/mail' )
-        .then( (response)=>{
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response)=> {
             // variabile singola che verrà trasformata in un array in un secondo momento
             //console.log(response)
             let emailSingola = response.data.response
             this.emailArray.push( emailSingola )
-            } )
+            })
         }
     },
     beforeUpdate() {
